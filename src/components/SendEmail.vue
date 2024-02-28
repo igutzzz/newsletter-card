0cafe7d085b6b151007c9a31c20deaff648e8c33
Vinicius Luna <viniciusluna@yahoo.com>
2024-02-28 17:51:44 -0300

    feat: add button email

---
+++ src/components/SendEmail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button>Inscrever-se na newsletter</button>
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+button {
+    background-color: #226CE0;
+    color: #FFFFFF;
+    font-weight: 600;
+    margin: 2em 0em;
+    border: none;
+    border-radius: 10px;
+    min-width: 300px;
+    height: 50px;
+    font-size: 15px;
+}
+</style>
