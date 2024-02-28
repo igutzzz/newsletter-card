941fd52c4a98c2172ab759053968632f793bc1fa
Vinicius Luna <viniciusluna@yahoo.com>
2024-02-28 17:51:21 -0300

    feat: add input style

--- src/components/EmailInput.vue
+++ src/components/EmailInput.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <label for="email">E-mail</label>
-        <br>
         <input type="email" name="email" id="email" placeholder="Digite o seu e-mail">
     </div>
 </template>
@@ -14,6 +13,22 @@
 
 <style scoped> 
     /* Estilizar o input de e-mail */
+    label {
+        font-weight: 500;
+        color: black;
+        display: block;
+    }
     input {
+        border-color: #D0D5DD;
+        border: 2px solid;
+        padding: 10px 14px;
+        outline: none;
+        background-color: transparent;
+        border-radius: 10px;
+        color: #667085;
+        font-weight: 400;
+        font-size: 14px;
+        margin-top: 8px;
+        min-width: 300px;
     }
 </style>
